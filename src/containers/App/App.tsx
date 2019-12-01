4c41b239845a59699b0780531b3bd855d0c2c10b
Alexey Kokhanovsky <awfj837@gmail.com>
2019-12-01 18:59:25 +0300

    add Material-UI, sass

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./App.module.scss";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <CssBaseline />
+      <div className={styles.root}>asfASFSA</div>
+    </>
+  );
+};
+
+export default App;
