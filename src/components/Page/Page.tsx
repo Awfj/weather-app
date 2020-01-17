b06f2105a9842ab262cd9629b0e03f34753eb437
Alexey Kokhanovsky <awfj837@gmail.com>
2020-01-17 11:15:22 +0300

    add Page

---
+++ src/components/Page/Page.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import AppDrawer from "../../components/AppDrawer/AppDrawer";
+import AppContent from "../../components/AppContent/AppContent";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      "& h2": {
+        marginBottom: theme.spacing(2)
+      },
+      "& h3": {
+        marginBottom: theme.spacing(1.5)
+      },
+      "& button": {
+        "&:focus": {
+          backgroundColor: theme.palette.action.selected
+        },
+        "&:hover": {
+          backgroundColor: theme.palette.action.hover
+        },
+        "&:active": {
+          backgroundColor: theme.palette.action.active,
+          color: theme.palette.primary.main
+        }
+      }
+    }
+  })
+);
+
+const Page = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppDrawer />
+      <AppContent />
+    </div>
+  );
+};
+
+export default Page;
--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 
-import {
-  ThemeProvider,
-  createStyles,
-  makeStyles,
-  Theme
-} from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { StylesProvider } from "@material-ui/styles";
 
-import AppDrawer from "../../components/AppDrawer/AppDrawer";
-import AppContent from "../../components/AppContent/AppContent";
+import Page from "../../components/Page/Page";
 
 import { lightTheme, darkTheme } from "../../theme";
 import useSettings from "../../hooks/useSettings";
@@ -18,39 +12,7 @@ import { SettingsDispatchCtx, SettingsCtx } from "../../contexts";
 import useWindowWidth from "../../hooks/useWindowWidth";
 import { WindowWidthCtx } from "../../contexts";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: "flex",
-      "& h2": {
-        marginBottom: "1rem"
-      },
-      "& h3": {
-        marginBottom: "0.75rem"
-      },
-      "& button, & a": {
-        "&:focus": {
-          backgroundColor: theme.palette.action.selected
-        },
-        "&:hover": {
-          backgroundColor: theme.palette.action.hover
-        },
-        "&:active": {
-          backgroundColor: theme.palette.action.active,
-          color: theme.palette.primary.contrastText
-        }
-      },
-      "& a:active": {
-        "& *": {
-          color: theme.palette.primary.contrastText
-        }
-      }
-    }
-  })
-);
-
 const App: React.FC = () => {
-  const classes = useStyles();
   const windowWidth = useWindowWidth();
   const [settings, dispatchSettings] = useSettings();
 
@@ -62,10 +24,7 @@ const App: React.FC = () => {
         <WindowWidthCtx.Provider value={windowWidth}>
           <SettingsDispatchCtx.Provider value={dispatchSettings}>
             <SettingsCtx.Provider value={settings}>
-              <div className={classes.root}>
-                <AppDrawer />
-                <AppContent />
-              </div>
+              <Page />
             </SettingsCtx.Provider>
           </SettingsDispatchCtx.Provider>
         </WindowWidthCtx.Provider>
