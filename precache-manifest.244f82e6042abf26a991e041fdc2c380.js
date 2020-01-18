7a3a975930ed204dfda07ebd5779cc62e8dbe969
Alexey Kokhanovsky <awfj837@gmail.com>
2020-01-18 13:34:40 +0300

    Updates

---
+++ precache-manifest.244f82e6042abf26a991e041fdc2c380.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c88630e7fcd81b2a6c2fff15b8ef083",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "b79ea0fc247231abd3c6",
+    "url": "/weather-app/static/css/main.db2c914a.chunk.css"
+  },
+  {
+    "revision": "e9e61141cabef64815ab",
+    "url": "/weather-app/static/js/2.29bce4f1.chunk.js"
+  },
+  {
+    "revision": "b79ea0fc247231abd3c6",
+    "url": "/weather-app/static/js/main.8f656913.chunk.js"
+  },
+  {
+    "revision": "ae340ef62e5fce871bc7",
+    "url": "/weather-app/static/js/runtime-main.d1454c76.js"
+  }
+]);
